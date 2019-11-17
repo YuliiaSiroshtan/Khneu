3bbc02af88d4e55e001fd5e8b340fc5cd677ac53
Yuliia Siroshtan <ysiroshtan1@gmail.com>
2019-11-17 18:52:31 +0200

    merge with frontend

--- Planner/Startup.cs
+++ Planner/Startup.cs
@@ -92,13 +92,6 @@ namespace Planner
       app.UseStaticFiles();
       app.UseAuthentication();
       app.UseMvc();
-
-
-      app.Run(async (context) =>
-      {
-        context.Response.ContentType = "text/html";
-        await context.Response.SendFileAsync(Path.Combine(env.WebRootPath, "src", "index.html"));
-      });
     }
   }
 
