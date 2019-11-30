d9e83ebb033a0712b7721450359866bfeb9a9897
Yuliia Siroshtan <ysiroshtan1@gmail.com>
2019-11-30 21:11:13 +0200

    fix: add child router  outlet

--- Planner/src/app/app.module.ts
+++ Planner/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedAppModule } from "src/app/shared/shared.module";
 import { CommonModule } from "@angular/common";
-import { AppHeaderComponent } from "src/app/common/app-header-component/app-header.component";
-import { AppSidenavComponent } from "src/app/common/app-sidenav-component/app-sidenav.component";
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-      AppComponent,
-      AppHeaderComponent,
-      AppSidenavComponent
+      AppComponent
   ],
   imports: [
       BrowserModule,
