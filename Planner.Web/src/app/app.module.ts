8071befdc70af6a4fa690787973f48e3189a60f3
Yuliia Siroshtan <ysiroshtan1@gmail.com>
2020-05-03 19:57:42 +0300

    fixes menu, adds footer

--- Planner.Web/src/app/app.module.ts
+++ Planner.Web/src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth-module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- Planner.Web/src/app/shared/components/header/header.component.ts
+++ Planner.Web/src/app/shared/components/header/header.component.ts
@@ -1,37 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { UtilsService } from '../../services/utils.service';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pl-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  get isHeadOfDepartmen(){
-    return this._utilsService.isHeadOfDepartmen;
-  }
-
-  get isTeacher(){
-    return this._utilsService.isTeacher;
-  }
-
-  get isTrainingDivision(){
-    return this._utilsService.isTrainingDivision;
-  }
-
-  constructor(
-    private _utilsService: UtilsService,
-    private _router: Router
-    ) { }
-
-  ngOnInit(): void {
-  }
-
-  logout(){
-    localStorage.clear();
-    this._router.navigate(['/login']);
-  }
+export class HeaderComponent {
 
 }
